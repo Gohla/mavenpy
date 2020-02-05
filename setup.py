e64d7ab14feeb874cdd6a2deacb5b63fdb56c8d6
Gabriel Konat <gabrielkonat@gmail.com>
2020-02-05 13:07:14 +0100

    Update version and Travis configuration.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
   name='mavenpy',
-  version='0.1.2',
+  version='0.1.3',
   description='Wrapper for calling Maven from Python',
   url='http://github.com/Gohla/mavenpy',
   author='Gabriel Konat',
