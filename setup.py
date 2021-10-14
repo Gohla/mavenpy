afaf33e5ff1736dc4b91ebe3ec0642ba183058e1
Gabriel Konat <gabrielkonat@gmail.com>
2021-10-14 11:31:07 +0200

    Set version to 0.1.5.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
   name='mavenpy',
-  version='0.1.4',
+  version='0.1.5',
   description='Wrapper for calling Maven from Python',
   url='http://github.com/Gohla/mavenpy',
   author='Gabriel Konat',
